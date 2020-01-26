f74f29477398606d0735f7b39e0ac4b5acbbe048
Tarriamr <pawel.putala@gmail.com>
2020-01-26 09:55:10 +0100

    Trójkaty równoramienne

--- src/main/java/pl/sda/javakrk25/day6/Ex_12/Ex_12_array_intersect.java
+++ src/main/java/pl/sda/javakrk25/day6/Ex_12/Ex_12_array_intersect.java
@@ -3,24 +3,25 @@ package pl.sda.javakrk25.day6.Ex_12;
 public class Ex_12_array_intersect {
 
     /**
-     * Napisać metodę, która zwraca część wsolną z dwóch tablic.
+     * Napisać metodę, która zwraca część wsolną z trzech tablic.
      *
      * @param args
      */
     public static void main(String[] args) {
         int[] tab1 = {1, 2, 3, 4, 5};
         int[] tab2 = {4, 5, 6, 7};
-        int[] tabFinal = getIntersect(tab1, tab2);
+        int[] tab3 = {4, 3, 7, 8, 9};
+        int[] tabFinal = getIntersect(tab1, tab2, tab3);
 
         for (int i : tabFinal) {
             System.out.printf("%d ", i);
         }
     }
 
-    private static int[] getIntersect(int[] tab1, int[] tab2) {
+    private static int[] getIntersect(int[] tab1, int[] tab2, int[] tab3) {
         int[] result = new int[0];
         for (int i : tab1) {
-            if (ArrayUtil.contains(tab2, i)) {
+            if (ArrayUtil.contains(tab2, i) && ArrayUtil.contains(tab3, i)) {
                 result = ArrayUtil.addToEnd(result, i);
             }
         }
