b3a86a88fd365fb175789dadcaf50c5e24bc2545
Tarriamr <pawel.putala@gmail.com>
2020-01-26 09:23:06 +0100

    Trójkaty równoramienne

---
+++ src/main/java/pl/sda/javakrk25/day6/Ex_12/Ex_12_array_intersect.java
@@ -0,0 +1,12 @@
+package pl.sda.javakrk25.day6.Ex_12;
+
+public class Ex_12_array_intersect {
+    /**
+     * Napisać metodę, która zwraca część wsolną z dwóch tablic.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
