99ca47aee58b4cbc3747e5a4ddf77412e89a9dcf
Tarriamr <pawel.putala@gmail.com>
2020-01-31 01:11:42 +0100

    Trójkaty równoramienne

--- src/main/java/pl/sda/javakrk25/day6/Ex_21/Ex_21_suma_cyfr.java
+++ src/main/java/pl/sda/javakrk25/day6/Ex_21/Ex_21_suma_cyfr.java
@@ -1,5 +1,7 @@
 package pl.sda.javakrk25.day6.Ex_21;
 
+import java.util.Scanner;
+
 public class Ex_21_suma_cyfr {
 
     /**
@@ -8,4 +10,28 @@ public class Ex_21_suma_cyfr {
      * obliczmy osobno sumę cyfr pazystych i nieparzystych
      * obliczmy stosunek sredniej arytmetycznej liczb parzystych od nieparzystych
      */
+
+    public static void main(String[] args) {
+
+        System.out.printf("Suma cyfr podanej liczbt to: %d.", sum(setLiczba()));
+    }
+
+    private static String setLiczba() {
+        String number;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj liczbę: ");
+        return number = scanner.next();
+    }
+
+    private static int sum(String liczba) {
+        int suma = 0;
+        char[] cyfra = liczba.toCharArray();
+
+        for (int i = 0; i < cyfra.length; i++) {
+            suma += cyfra[i];
+        }
+        return suma;
+    }
 }
